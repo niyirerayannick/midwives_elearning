24ebc919711942693645b5517b524b31acbfab0f
niyirerayannick <niyannick120@gmail.com>
2024-10-11 11:50:16 +0200

    yes

--- midwives_elearning/settings.py
+++ midwives_elearning/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'midwives_elearning.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'database.sqlite3',
+        'NAME': BASE_DIR / 'database/database.sqlite3',
     }
 }
 
